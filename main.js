805159fd15de42d80f0ed32c5c006520da0d26c4
konradkecinski <szachkonrad@op.pl>
2020-09-10 00:13:20 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Config variables
-const SIGNALING_SERVER_URL = 'http://localhost';
+const SIGNALING_SERVER_URL = 'http://localhost'.contact(process.env.PORT.toString());
 const PC_CONFIG = {};
 
 let socket = io(SIGNALING_SERVER_URL, { autoConnect: false });
